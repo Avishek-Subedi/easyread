dfa2f5a0706ff9f1faed433f771a437ca76f9dff
Avishek Subedi <avisheksubedi0@gmail.com>
2022-01-26 14:47:48 +0545

    implementaiton of slider

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
 
@@ -16,8 +16,33 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  double _fontsize = 40;
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            Expanded(
+              child: TextField(
+                textInputAction: TextInputAction.done,
+                style: TextStyle(fontSize: _fontsize),
+                maxLines: null,
+              ),
+            ),
+            Slider(
+              value: _fontsize,
+              onChanged: (newsize) {
+                setState(() {
+                  _fontsize = newsize;
+                });
+              },
+              min: 30,
+              max: 200,
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
